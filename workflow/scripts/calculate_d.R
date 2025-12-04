9442e5021017166f5227455ff3074022e0c47dde
Clara Leiva <vogonapoesia@gmail.com>
2025-12-04 20:13:14 +0100

    Apply make.names() to annot rownames to match tree tip labels in calculate_d.R

--- workflow/scripts/calculate_d.R
+++ workflow/scripts/calculate_d.R
@@ -73,6 +73,8 @@ annot[,1:3] <- NULL
 #genepa <- NULL
 annot <- t(annot)
 annot <- as.data.frame(annot)
+# Aplicar make.names() a los rownames para que coincidan con los tips del árbol
+rownames(annot) <- make.names(rownames(annot))
 annot$Id <- rownames(annot)
 #Double check that each column has 2 states
 remove <- vector()
